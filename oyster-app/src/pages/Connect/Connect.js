06dd0b0f0ba86961a082216ad6ba0c07ad63c73c
Nancy Yao <nancy.yao2@gmail.com>
2018-03-04 03:43:38 -0500

    Matches

--- oyster-app/src/pages/Connect/Connect.js
+++ oyster-app/src/pages/Connect/Connect.js
@@ -9,14 +9,14 @@ import './Connect.css';
 const Connect = (props) => {
   return (
     <div>
-    <div class="eventCarousel">
-      <h1 class="title">GirlsLEAD Summit 2018 - Fri March 9 7:30AM</h1>
-      <ConnectEvent1 />
-    </div>
-    <div class="eventCarousel">
-      <h1 class="title">Eastern High School Tree Planting - Sat March 31 9:00AM</h1>
-      <ConnectEvent2 />
-    </div>
+      <div class="eventCarousel">
+        <h1 class="title">GirlsLEAD Summit 2018 - Fri March 9 7:30AM</h1>
+        <ConnectEvent1 />
+      </div>
+      <div class="eventCarousel">
+        <h1 class="title">Eastern High School Tree Planting - Sat March 31 9:00AM</h1>
+        <ConnectEvent2 />
+      </div>
       <br></br><br></br>
     </div>
   )
