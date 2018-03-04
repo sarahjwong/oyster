6ab05e5edf91e0af5af5917be276ede5df5d8c4f
sarahjwong <sarahwong2019@u.northwestern.edu>
2018-03-04 12:40:06 +0000

    sign up form

--- oyster-app/src/components/TextForm/TextForm.js
+++ oyster-app/src/components/TextForm/TextForm.js
@@ -1,89 +1,25 @@
-import React from 'react'
-import { FieldGroup } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import "./TextForm.css";
+class TextForm extends Component {
+ render () {
+   return (
+     <form className= "demoForm">
 
-function FieldGroup({ id, label, help, ...props }) {
-  return (
-    <FormGroup controlId={id}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
-    </FormGroup>
-  );
+       <div className="form-group">
+         <label htmlFor="email">Email address</label>
+         <input type="email" className="form-control"
+           name="email" />
+       </div>
+       <div className="form-group">
+         <label htmlFor="password">Password</label>
+         <input type="password" className="form-control"
+           name="password" />
+       </div>
+       <button type="submit" className="btn btn-primary submit-btn">
+          Sign up
+       </button>
+     </form>
+   )
+ }
 }
-
-const formInstance = (
-  <form>
-    <FieldGroup
-      id="formControlsText"
-      type="text"
-      label="Text"
-      placeholder="Enter text"
-    />
-    <FieldGroup
-      id="formControlsEmail"
-      type="email"
-      label="Email address"
-      placeholder="Enter email"
-    />
-    <FieldGroup id="formControlsPassword" label="Password" type="password" />
-    <FieldGroup
-      id="formControlsFile"
-      type="file"
-      label="File"
-      help="Example block-level help text here."
-    />
-
-    <Checkbox checked readOnly>
-      Checkbox
-    </Checkbox>
-    <Radio checked readOnly>
-      Radio
-    </Radio>
-
-    <FormGroup>
-      <Checkbox inline>1</Checkbox> <Checkbox inline>2</Checkbox>{' '}
-      <Checkbox inline>3</Checkbox>
-    </FormGroup>
-    <FormGroup>
-      <Radio name="radioGroup" inline>
-        1
-      </Radio>{' '}
-      <Radio name="radioGroup" inline>
-        2
-      </Radio>{' '}
-      <Radio name="radioGroup" inline>
-        3
-      </Radio>
-    </FormGroup>
-
-    <FormGroup controlId="formControlsSelect">
-      <ControlLabel>Select</ControlLabel>
-      <FormControl componentClass="select" placeholder="select">
-        <option value="select">select</option>
-        <option value="other">...</option>
-      </FormControl>
-    </FormGroup>
-    <FormGroup controlId="formControlsSelectMultiple">
-      <ControlLabel>Multiple select</ControlLabel>
-      <FormControl componentClass="select" multiple>
-        <option value="select">select (multiple)</option>
-        <option value="other">...</option>
-      </FormControl>
-    </FormGroup>
-
-    <FormGroup controlId="formControlsTextarea">
-      <ControlLabel>Textarea</ControlLabel>
-      <FormControl componentClass="textarea" placeholder="textarea" />
-    </FormGroup>
-
-    <FormGroup>
-      <ControlLabel>Static text</ControlLabel>
-      <FormControl.Static>email@example.com</FormControl.Static>
-    </FormGroup>
-
-    <Button type="submit">Submit</Button>
-  </form>
-);
-
-render(formInstance);
+export default TextForm;
--- oyster-app/src/pages/Home/Home.js
+++ oyster-app/src/pages/Home/Home.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import EventButton from '../../components/EventButton/EventButton';
+import TextForm from '../../components/TextForm/TextForm'
 import './Home.css';
 
 const Home = (props) => {
   return (
     <div>
-      <h1 className="titleHeader"> WELCOME </h1>
-    </div>
+
+      <h1 className="titleHeader"> Sign Up </h1>
+      <br>
+      <TextForm />
+
+      </div>
   )
 }
 
-export default Home
+export default Home;
