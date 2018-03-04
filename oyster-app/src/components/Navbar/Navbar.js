a42d8819bceb461efe3b9401ebefa5252b426940
sarahjwong <sarahwong2019@u.northwestern.edu>
2018-03-04 04:36:16 +0000

    logo and font

--- oyster-app/src/components/Navbar/Navbar.js
+++ oyster-app/src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import '../../App.css';
 
 
 const NavBar = (props) => {
   return (
     <Navbar>
           <Navbar.Header>
+          <img id="logo" src={require('../../images/oyster-logo.png')}></img>
+
           <Navbar.Brand>
               <a href="/">Oyster</a>
           </Navbar.Brand>
